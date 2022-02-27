a7511fc265f5eb952ba2b1f074d6ae7fe9de30bf
V914eslav <v914-working@rambler.ru>
2022-02-27 13:35:04 +0300

    create routes

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-// import styles from "./App.module.css";
-import PeoplePage from "@containers/PeoplePage";
+import { Routes, Route, NavLink, BrowserRouter } from "react-router-dom";
 
-// import { getApiresource } from "../../utils/network";
+import PeoplePage from "@containers/PeoplePage";
+import HomePage from "@containers/HomePage";
+import routesConfig from "../../routes/routesConfig";
+
+// import styles from "./App.module.css";
+
 const App = () => {
-  return <PeoplePage />;
+  return (
+    <>
+      <BrowserRouter>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/people">Poeple</NavLink>
+        {/* <Routes>
+          {routesConfig.map((route, index) => {
+            <Route
+              key={index}
+              path={route.path}
+              element={route.element}
+             
+            />;
+          })}
+        </Routes> */}
+
+        <Routes>
+          <Route path="/" element={<HomePage />} exact />
+          <Route path="/people" element={<PeoplePage exact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
