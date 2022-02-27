da1b191a36d749bde430105a7cb7dfd869e4da47
V914eslav <v914-working@rambler.ru>
2022-02-27 17:41:47 +0300

    add styles

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-// import PeoplePage from "@containers/PeoplePage";
-// import HomePage from "@containers/HomePage";
 import Header from "@components/Header";
 import routesConfig from "@routes/routesConfig";
 
-// import styles from "./App.module.css";
+import styles from "./App.module.css";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
+      <div className={styles.wrapper}>
+        <BrowserRouter>
+          <Header />
 
-        <Routes>
-          {routesConfig.map((route, index) => (
-            <Route key={index} path={route.path} element={route.element} />
-          ))}
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            {routesConfig.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 };
