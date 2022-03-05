e51be15346941459579faa3741ccb306fb99ad78
V914eslav <v914-working@rambler.ru>
2022-03-05 19:04:18 +0300

    forned the requst and styled the component

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,18 +1,65 @@
 import React from "react";
+import { useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
+
+import { withErrorApi } from "@hoc-helpers/withErrorApi";
+import { getApiResource } from "@utils/network";
+import { API_PERSONE } from "@constants/api";
 import PropTypes from "prop-types";
 
 import styles from "./PersonPage.module.css";
 
-const PersonPage = () => {
+const PersonPage = ({ setErrorApi }) => {
+  const [personInfo, setPersonInfo] = useState(null);
+  const [personName, setPersonName] = useState(null);
   const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getApiResource(`${API_PERSONE}/${id}/`);
+      if (res) {
+        setPersonInfo([
+          { title: "Height", data: res.height },
+          { title: "Mass", data: res.mass },
+          { title: "Hair Color", data: res.hair_color },
+          { title: "Skin Color", data: res.skin_color },
+          { title: "Eye Color", data: res.eye_color },
+          { title: "Birth Year", data: res.birth_year },
+          { title: "Gender", data: res.gender },
+        ]);
+        setPersonName(res.name);
+        // res.films
+        setErrorApi(false);
+      } else {
+        setErrorApi(true);
+      }
+    })();
+  }, []);
+
   return (
     <>
-      <h1 className={styles.hhh}>{id}</h1>
+      <h1 className={styles.hhh}>{personName}</h1>
+      {personInfo && (
+        <ul>
+          {personInfo.map(
+            ({ title, data }) =>
+              data && (
+                <li key={title}>
+                  <span>
+                    {title}:{data}
+                  </span>
+                </li>
+              )
+          )}
+        </ul>
+      )}
     </>
   );
 };
 
-PersonPage.propTypes = {};
+PersonPage.propTypes = {
+  setErrorApi: PropTypes.func,
+};
 
-export default PersonPage;
+export default withErrorApi(PersonPage);
