5562616800599023541050cc3ae98d771e77d23d
V914eslav <v914-working@rambler.ru>
2022-03-15 19:25:04 +0300

    create Favorites Page

--- src/containers/FavotitesPage/FavotitesPage.jsx
+++ src/containers/FavotitesPage/FavotitesPage.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
-// import styles from "./FavotitesPage.module.css";
+import PeopleList from "@components/PeoplePage/PeopleList";
+import styles from "./FavotitesPage.module.css";
 const FavotitesPage = () => {
+  const [people, setPeople] = useState([]);
+
   const storeData = useSelector((state) => state.favoriteReducer);
 
-  return <div>FavotitesPage</div>;
+  useEffect(() => {
+    const arr = Object.entries(storeData);
+    if (arr.length) {
+      const res = arr.map((item) => {
+        return {
+          id: item[0],
+          ...item[1],
+        };
+      });
+      setPeople(res);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1 className="header__text">Favorite Page</h1>
+      {people.length ? (
+        <PeopleList people={people} />
+      ) : (
+        <h2 className={styles.comment}>No data</h2>
+      )}
+    </>
+  );
 };
 
 export default FavotitesPage;
