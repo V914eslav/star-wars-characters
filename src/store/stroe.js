9fbcf7099dd98c70d7e454adebf097917677f86e
V914eslav <v914-working@rambler.ru>
2022-03-16 19:11:36 +0300

    create localStorage

--- src/store/stroe.js
+++ src/store/stroe.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { setLocalStorage } from "@utils/localStorage";
 import rootREducer from "./reducers";
 
 const store = createStore(
   rootREducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+store.subscribe(() => {
+  setLocalStorage("store", store.getState().favoriteReducer);
+});
 export default store;
