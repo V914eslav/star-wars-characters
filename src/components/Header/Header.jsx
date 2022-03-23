ea3053632e0fabffe4eebc44ed68a23f136b8252
V914eslav <v914-working@rambler.ru>
2022-03-23 12:49:30 +0300

    add headerIcon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import {
+  useTheme,
+  THEME_LIGHT,
+  THEME_DARK,
+  THEME_NEITRAL,
+} from "@context/ThemeProvider";
 import Favorite from "@components/Favorite";
+
+import imgDroid from "./img/droid.svg";
+import imgLightSaber from "./img/lightsaber.svg";
+import imgSpaceStation from "./img/space-station.svg";
 
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [icon, setIcon] = useState(imgSpaceStation);
+  const isTheme = useTheme();
+
+  useEffect(() => {
+    switch (isTheme.theme) {
+      case THEME_LIGHT:
+        setIcon(imgSpaceStation);
+        break;
+      case THEME_DARK:
+        setIcon(imgLightSaber);
+        break;
+      case THEME_NEITRAL:
+        setIcon(imgDroid);
+        break;
+      default:
+        setIcon(imgSpaceStation);
+    }
+  }, [isTheme]);
+
   return (
     <div className={styles.contianer}>
+      <img src={icon} alt="Star Wars" className={styles.logo} />
+
       <ul className={styles.list__contianer}>
         <li className={styles.list__item}>
           <NavLink to="/">Home</NavLink>
