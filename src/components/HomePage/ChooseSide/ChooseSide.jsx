9cb42b78c78d1e363229a195397490213e0f16c9
V914eslav <v914-working@rambler.ru>
2022-03-25 11:26:31 +0300

    create ChooseSideItem

--- src/components/HomePage/ChooseSide/ChooseSide.jsx
+++ src/components/HomePage/ChooseSide/ChooseSide.jsx
@@ -5,21 +5,46 @@ import {
   THEME_DARK,
   THEME_NEITRAL,
 } from "@context/ThemeProvider";
+import PropTypes from "prop-types";
 
-const ChooseSide = () => {
+import imgLightSide from "./img/light-side.jpg";
+import imgDarkSide from "./img/dark-side.jpg";
+import imgFalcon from "./img/falcon.jpg";
+
+import styles from "./ChooseSide.module.css";
+
+const ChooseSideItem = ({ theme, text, img }) => {
   const isTheme = useTheme();
 
   return (
+    <div onClick={() => isTheme.change(theme)} className={styles.item}>
+      <div className={styles.item__header}>{text}</div>
+      <img src={img} alt={text} className={styles.item__img} />
+    </div>
+  );
+};
+ChooseSideItem.propTypes = {
+  theme: PropTypes.string,
+  text: PropTypes.string,
+  img: PropTypes.string,
+};
+
+const ChooseSide = () => {
+  return (
     <>
-      {isTheme.theme}
-      <button onClick={() => isTheme.change(THEME_LIGHT)}>Light</button>
-      <button onClick={() => isTheme.change(THEME_DARK)}>Dark</button>
-      <button onClick={() => isTheme.change(THEME_NEITRAL)}>Neitral</button>
+      <ChooseSideItem
+        theme={THEME_LIGHT}
+        text="Light Side"
+        img={imgLightSide}
+      />
+      <ChooseSideItem theme={THEME_DARK} text="Dark Side" img={imgDarkSide} />
+      <ChooseSideItem
+        theme={THEME_NEITRAL}
+        text="I'm Han Solo"
+        img={imgFalcon}
+      />
     </>
   );
 };
 
-ChooseSide.protoTypes = {
-  //text: PropTypes.string
-};
 export default ChooseSide;
