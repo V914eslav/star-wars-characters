744d401b2785bbcc0414d506542889dfa315616d
V914eslav <v914-working@rambler.ru>
2022-04-06 11:47:48 +0300

    add cancle clear

--- src/components/UI/UiInput/UiInput.jsx
+++ src/components/UI/UiInput/UiInput.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import cn from "classnames";
+
+import icon from "./img/cancel.svg";
+
 import "../index.css";
 import styles from "./UiInput.module.css";
 const UiInput = ({ value, handleInputChange, placeholder, classes }) => {
@@ -13,6 +16,12 @@ const UiInput = ({ value, handleInputChange, placeholder, classes }) => {
         onChange={(e) => handleInputChange(e.target.value)}
         placeholder={placeholder}
         className={styles.input}
+      />
+      <img
+        onClick={() => value && handleInputChange("")}
+        src={icon}
+        className={cn(styles.clear, !value && styles.clear__disabled)}
+        alt="clear"
       />
     </div>
   );
